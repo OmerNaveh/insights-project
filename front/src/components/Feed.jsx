f9725043bbb4723dc03d3564c08a9a08c4cff293
Omer Naveh <89572395+OmerNaveh@users.noreply.github.com>
2022-02-09 18:01:26 +0200

    added omni search bar with debounce

--- front/src/components/Feed.jsx
+++ front/src/components/Feed.jsx
@@ -1,12 +1,23 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import context from "../context/scrapingContext";
 import Post from "./Post";
+import SearchBar from "./SearchBar";
 export default function Feed() {
   const { data } = useContext(context);
+  const [localData, setLocalData] = useState([]);
   const renderFeed = () => {
+    if (localData.length > 0)
+      return localData.map((entry) => (
+        <Post key={entry.date + entry.title} entry={entry} />
+      ));
     return data.map((entry) => (
       <Post key={entry.date + entry.title} entry={entry} />
     ));
   };
-  return <div className="feed">{renderFeed()}</div>;
+  return (
+    <div>
+      <SearchBar setLocalData={setLocalData} localData={localData} />
+      <div className="feed">{renderFeed()}</div>
+    </div>
+  );
 }
---
+++ front/src/components/SearchBar.jsx
@@ -0,0 +1,38 @@
+import React, { useContext, useRef } from "react";
+import context from "../context/scrapingContext";
+import _ from "lodash";
+export default function SearchBar({ setLocalData, localData }) {
+  const { data } = useContext(context);
+  const filterValues = useRef(null);
+  const filterLocally = () => {
+    const filterBy = filterValues.current.value;
+    const filteredArr = data.filter(
+      (entry) =>
+        entry.title.toLowerCase().includes(filterBy.toLowerCase()) ||
+        entry.content.toLowerCase().includes(filterBy.toLowerCase()) ||
+        entry.author.toLowerCase().includes(filterBy.toLowerCase())
+    );
+    setLocalData(filteredArr.length ? filteredArr : []);
+    errorMessage(filteredArr, filterBy);
+  };
+
+  const errorMessage = (data, filterVal) => {
+    if (data.length === 0 && filterVal) {
+      document.querySelector(".searchError").textContent = "Not Found";
+    } else {
+      document.querySelector(".searchError").textContent = "";
+    }
+  };
+  const debouncedFilter = _.debounce(filterLocally, 300);
+  return (
+    <div>
+      <input
+        ref={filterValues}
+        onKeyDown={debouncedFilter}
+        className="searchBar"
+        placeholder="Search"
+      ></input>
+      <div className="searchError"></div>
+    </div>
+  );
+}
